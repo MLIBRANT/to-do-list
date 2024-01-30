e3c029180755dae928deedfea2ca22bd706e757f
Malgorzata Librant <librantgosia@gmail.com>
2024-01-30 08:36:11 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,26 +10,41 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li>
-              ${task.content}
-            </li> 
-            `; 
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            <button class="js-remove">delete</button>
+              ${task.content}
+            </li> 
+            `; 
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+       
+        removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+        removeTask(index)
+
+        });
+        });
     };
 
     const onFormSubmit = (event) => {
